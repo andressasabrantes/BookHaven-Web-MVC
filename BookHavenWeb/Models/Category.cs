5ec83f02b3cd676be0f83d86c074fd48a9ff4b57
Andressa Abrantes <andressaabrantes@gmail.com>
2023-05-02 15:39:31 -0300

    Display Name and Range Validation

--- BookHavenWeb/Models/Category.cs
+++ BookHavenWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection.Metadata.Ecma335;
 
 namespace BookHavenWeb.Models
@@ -9,8 +10,10 @@ namespace BookHavenWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The name should be between 1 and 100.")]
+
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
-
     }
 }
