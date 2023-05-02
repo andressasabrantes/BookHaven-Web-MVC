88a7533bb0a9943c8c1171c6ba7a12587e51dfba
Andressa Abrantes <andressaabrantes@gmail.com>
2023-05-02 14:57:42 -0300

    Custom Validation

--- BookHavenWeb/Controllers/CategoryController.cs
+++ BookHavenWeb/Controllers/CategoryController.cs
@@ -31,6 +31,10 @@ namespace BookHavenWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
             if(ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
