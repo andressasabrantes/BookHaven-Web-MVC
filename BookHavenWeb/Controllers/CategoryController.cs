24336e7d0e8e912acf600debf78e6dffa7896b04
Andressa Abrantes <andressaabrantes@gmail.com>
2023-05-02 11:43:24 -0300

    Adding bootstrap theme

---
+++ BookHavenWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BookHavenWeb.Data;
+using BookHavenWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookHavenWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ BookHavenWeb/Migrations/20230502115210_AddDatabaseToLocalWork.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookHavenWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDatabaseToLocalWork : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
