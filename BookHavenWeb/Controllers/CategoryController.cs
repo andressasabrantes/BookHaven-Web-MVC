2f465fb46fd5485eccf6023aaf4354878c79ff0d
Andressa Abrantes <andressaabrantes@gmail.com>
2023-05-02 14:06:38 -0300

    Create Category

--- BookHavenWeb/Controllers/CategoryController.cs
+++ BookHavenWeb/Controllers/CategoryController.cs
@@ -25,5 +25,15 @@ namespace BookHavenWeb.Controllers
         {
             return View();
         }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
